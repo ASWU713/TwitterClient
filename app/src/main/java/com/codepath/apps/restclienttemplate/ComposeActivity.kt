d5d79b863e9c717f9d38d2be651ea1927ba23b3d
Andrew Wu <awu@advancelocal.com>
2022-03-17 10:39:14 -0500

    5/5 required user stories completed

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.kt
@@ -1,12 +1,15 @@
   package com.codepath.apps.restclienttemplate
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import com.codepath.apps.restclienttemplate.models.Tweet
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
@@ -15,6 +18,7 @@ import okhttp3.Headers
 
     lateinit var etCompose: EditText
     lateinit var btnTweet: Button
+    lateinit var tvCharCount: TextView
 
     lateinit var client: TwitterClient
 
@@ -24,8 +28,17 @@ import okhttp3.Headers
 
         etCompose = findViewById(R.id.etTweetCompose)
         btnTweet = findViewById(R.id.btnTweet)
+        tvCharCount = findViewById(R.id.tvChars)
 
         client = TwitterApplication.getRestClient(this)
+
+        etCompose.addTextChangedListener{
+            val remainderChar = 280 - etCompose.text.length
+            tvCharCount.text = remainderChar.toString()
+            if(remainderChar < 0){
+                tvCharCount.setTextColor(Color.RED)
+            }
+        }
 
         //Handle user click on tweet button
         btnTweet.setOnClickListener{
@@ -37,7 +50,7 @@ import okhttp3.Headers
                 //look into displaying snackbar
             } else{
                 //2. Make sure tweet is under character count
-                if(tweetContent.length > 140){
+                if(tweetContent.length > 280){
                     Toast.makeText(this, "Tweet too long",Toast.LENGTH_LONG).show()
                 } else{
                     client.publishTweet(tweetContent, object: JsonHttpResponseHandler(){
